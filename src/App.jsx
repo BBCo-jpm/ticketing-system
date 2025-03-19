1276301df136168bd6864179ae184f7eefa77b54
BBCo-jpm <jmcgrath@nutracorp.com>
2025-03-19 16:58:35 -0600

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TicketingSystem from "./components/TicketingSystem";
+
+function App() {
+  return (
+    <div className="App">
+      <TicketingSystem />
+    </div>
+  );
+}
+
+export default App;
