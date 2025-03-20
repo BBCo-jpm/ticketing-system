ed62c7b2748e6a4f2d25ab11c31a2570ed12e471
BBCo-jpm <jmcgrath@nutracorp.com>
2025-03-20 11:11:38 -0600

    V12

--- src/components/TicketingSystem.tsx
+++ src/components/TicketingSystem.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "../TicketingSystem.css";
+import "./TicketingSystem.css";
 import { db } from "../config/firebaseConfig";
 import {
   collection,
